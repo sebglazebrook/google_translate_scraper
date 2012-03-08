4430379731c4ccee2d8478a17ddaed45377e0c24
Sebastian Glazebrook <betaass@gmail.com>
2012-03-08 13:42:43 +1100

    class variables created

--- lib/google_translate_scraper/translator.rb
+++ lib/google_translate_scraper/translator.rb
@@ -1,10 +1,16 @@
 module GoogleTranslateScraper
   class Translator
+    @@input_language
+    @@target_language
+    @@text
     
     def translate(input_language, target_language, text )
-      get_translation(input_language, target_language, text)
+      @@input_language = input_language
+      @@target_language = target_language
+      @@text = text
+      get_translation
     end
-    
+  
    
     
   end
