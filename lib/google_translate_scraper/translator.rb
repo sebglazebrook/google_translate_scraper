15fe152a21eb2719b352663a8dcd3f2055df80d7
Sebastian Glazebrook <betaass@gmail.com>
2012-03-08 13:40:13 +1100

    created basic translate method without error detection

--- lib/google_translate_scraper/translator.rb
+++ lib/google_translate_scraper/translator.rb
@@ -1,5 +1,11 @@
 module GoogleTranslateScraper
   class Translator
     
+    def translate(input_language, target_language, text )
+      get_translation(input_language, target_language, text)
+    end
+    
+   
+    
   end
 end
